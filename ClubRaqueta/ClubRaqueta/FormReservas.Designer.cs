ed77ef71d778374e56eb57b1e878ccdad472dbd9
Chema <chemaruiz94@gmail.com>
2021-11-26 10:48:56 +0100

    Commit - Diseño panel 1

---
+++ ClubRaqueta/ClubRaqueta/FormReservas.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace ClubRaqueta
+{
+    partial class FormReservas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_dni_soc = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.msk_txt_telefono = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_apellidos = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.msk_txt_cuenta_corriente = new System.Windows.Forms.MaskedTextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elige al socio:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(144, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(209, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(461, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DNI socio:";
+            // 
+            // lbl_dni_soc
+            // 
+            this.lbl_dni_soc.AutoSize = true;
+            this.lbl_dni_soc.Location = new System.Drawing.Point(551, 27);
+            this.lbl_dni_soc.Name = "lbl_dni_soc";
+            this.lbl_dni_soc.Size = new System.Drawing.Size(0, 17);
+            this.lbl_dni_soc.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txt_email);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.msk_txt_cuenta_corriente);
+            this.panel1.Controls.Add(this.txt_apellidos);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.msk_txt_telefono);
+            this.panel1.Controls.Add(this.txt_direccion);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txt_nombre);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(28, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(910, 189);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nombre:";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(106, 19);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(232, 22);
+            this.txt_nombre.TabIndex = 1;
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Location = new System.Drawing.Point(106, 67);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(232, 22);
+            this.txt_direccion.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Direccion:";
+            // 
+            // msk_txt_telefono
+            // 
+            this.msk_txt_telefono.Location = new System.Drawing.Point(106, 115);
+            this.msk_txt_telefono.Mask = "000-000-000";
+            this.msk_txt_telefono.Name = "msk_txt_telefono";
+            this.msk_txt_telefono.Size = new System.Drawing.Size(232, 22);
+            this.msk_txt_telefono.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Telefono:";
+            // 
+            // txt_apellidos
+            // 
+            this.txt_apellidos.Location = new System.Drawing.Point(473, 19);
+            this.txt_apellidos.Name = "txt_apellidos";
+            this.txt_apellidos.Size = new System.Drawing.Size(402, 22);
+            this.txt_apellidos.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(393, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Apellidos:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(393, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "CCC:";
+            // 
+            // msk_txt_cuenta_corriente
+            // 
+            this.msk_txt_cuenta_corriente.Location = new System.Drawing.Point(473, 67);
+            this.msk_txt_cuenta_corriente.Mask = "ES-00-0000-0000-00-0000000000 ";
+            this.msk_txt_cuenta_corriente.Name = "msk_txt_cuenta_corriente";
+            this.msk_txt_cuenta_corriente.Size = new System.Drawing.Size(402, 22);
+            this.msk_txt_cuenta_corriente.TabIndex = 8;
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(473, 118);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(402, 22);
+            this.txt_email.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(393, 118);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Email:";
+            // 
+            // FormReservas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 682);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbl_dni_soc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormReservas";
+            this.Text = "FormReservas";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_dni_soc;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_apellidos;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox msk_txt_telefono;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox msk_txt_cuenta_corriente;
+    }
+}
